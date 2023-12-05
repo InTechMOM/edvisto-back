c15ce6f8c110e12147cde5bdfcd32b33dc8dd0e3
Karen <karen-0212@hotmail.com>
2023-12-05 18:59:39 -0300

    add controllers

---
+++ src/api/controllers/feedback/patch.js
@@ -0,0 +1,50 @@
+import StudentsFeedback from '../../../models/feedback.js';
+import { schemaFeedback } from '../../validators/feedback.js';
+import isValidObjectId from '../../utils/valid.js';
+
+async function studentFeedback(request, response, next) {
+  try {
+    const id = request.params.id; //Id de entregable
+
+    isValidObjectId(id);
+
+    //Validación de datos
+    const { error, value } = schemaFeedback.validate(request.body);
+
+    if (!value.feedback?.skills) {
+      return response
+        .status(400)
+        .json({ error: 'Qualification must not be empty' });
+    }
+
+    if (error) {
+      return response.status(400).json({ error: error.details[0].message });
+    }
+
+    const feedback = await StudentsFeedback.findByIdAndUpdate(
+      id,
+      { feedback: request.body.feedback, qualified: true },
+      { new: true }
+    );
+    if (!feedback) {
+      return response.status(404).json({
+        message: 'Delivery Not Found',
+      });
+    }
+
+    return response.status(201).json({
+      message: 'Successfully classified project',
+      data: feedback,
+    });
+  } catch (error) {
+    if (error.message === 'Id Not Valid') {
+      return response.status(422).json({
+        message: 'Id Not Valid',
+      });
+    } else {
+      next(error);
+    }
+  }
+}
+
+export default studentFeedback;
