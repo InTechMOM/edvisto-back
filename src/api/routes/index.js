9f78cb119e6feec1d8d760122e27724056d0d749
Carolina <angiedylexx@gmail.com>
2023-12-05 18:35:29 -0500

    Se quita el api/ antes de lcada ruta en la documentación, se elimina esquema de swagger del archivo index.js

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -8,25 +8,6 @@ const router = Router();
 
 router.use('/docs', serve, setup(swaggerSpecification));
 
-/**
- * @openapi
- * /api/register:
- *  post:
- *   summary: The following route creates, stores and sends a verification email to the provided email
- *   requestBody:
- *    required: true
- *   responses:
- *    201:
- *     description: User Created
- *    400:
- *     description: Bad Request
- *    422:
- *     description: Validation error, wrong date format
- *    409:
- *     description: Email is assigned to another user, The password do not match
- *    500:
- *     description: Unknown error
- */
 router.use('/', usersRouter);
 
 export default router;
