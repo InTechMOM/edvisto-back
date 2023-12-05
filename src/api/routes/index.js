b87cab96108c40ae47f73ab5bf5f53acf4cfd7d7
Karen <karen-0212@hotmail.com>
2023-12-05 18:45:05 -0300

    add routers and docs

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,14 @@
-import { Router } from "express";
-import swaggerJsdoc from "swagger-jsdoc";
-import { serve, setup } from "swagger-ui-express";
-import swaggerSpecification from "../../config/swagger.js";
+import { Router } from 'express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerSpecification from '../../config/swagger.js';
+import assignmentRouter from './assignment.js';
 
 const router = Router();
 
-router.use("/docs", serve);
-router.get("/docs", setup(swaggerJsdoc(swaggerSpecification)));
+router.use('/docs', serve);
+router.get('/docs', setup(swaggerJsdoc(swaggerSpecification)));
+
+router.use('/', assignmentRouter);
 
 export default router;
